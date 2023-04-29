1ebfdb79eefba5471599d461042cc30e77c5a644
ROMARIC LAMARE <108812597+RomaricLamare@users.noreply.github.com>
2023-04-29 14:24:35 +0200

    userHandlers

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
+
+// const welcome = (req, res) => {
+//   res.send("Welcome to my favourite movie list");
+// };
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send("Welcome to my users list");
 };
 
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
+// const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers")
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
+// app.get("/api/movies", movieHandlers.getMovies);
+// app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.get("/api/users", userHandlers.getUsers)
+app.get("/api/users/:id", userHandlers.getUsersById)
 
 app.listen(port, (err) => {
   if (err) {
