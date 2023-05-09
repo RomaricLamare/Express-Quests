b7ed9db9605bb84b6ae26d7e25cf0c1c09c46813
ROMARIC LAMARE <108812597+RomaricLamare@users.noreply.github.com>
2023-05-09 16:51:35 +0200

    add a user with post

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json())
+
 const port = process.env.APP_PORT ?? 5000;
 
 // const welcome = (req, res) => {
@@ -18,9 +20,11 @@ app.get("/", welcome);
 // const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers")
 
+// app.post("/api/movies", movieHandlers.postMovie)
 // app.get("/api/movies", movieHandlers.getMovies);
 // app.get("/api/movies/:id", movieHandlers.getMovieById);
 
+app.post("/api/users", userHandlers.postUser)
 app.get("/api/users", userHandlers.getUsers)
 app.get("/api/users/:id", userHandlers.getUsersById)
 
--- movieHandlers.js
+++ movieHandlers.js
@@ -42,6 +42,8 @@ const getMovies = (req, res) => {
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
+
+
   database
     .query("select * from movies where id = ?", [id])
     .then(([movie]) => {
@@ -57,7 +59,26 @@ const getMovieById = (req, res) => {
 });
 }
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
+
+
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
 };
